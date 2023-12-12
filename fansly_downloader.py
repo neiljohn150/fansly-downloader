00cfb53aba57031c00a42b52e6ccd64452935b20
neiljohn150 <153593015+neiljohn150@users.noreply.github.com>
2023-12-11 23:44:24 -0800

    Fix for videos without audio

--- fansly_downloader.py
+++ fansly_downloader.py
@@ -638,12 +638,15 @@ def download_m3u8(m3u8_url: str, save_path: str):
     
     input_container = av.open(io.BytesIO(segment), format='mpegts')
     video_stream = input_container.streams.video[0]
-    audio_stream = input_container.streams.audio[0]
+    audio_stream = None
+    if (len(input_container.streams.audio) != 0):
+        audio_stream = input_container.streams.audio[0]
 
     # define output container and streams
     output_container = av.open(f"{save_path}.mp4", 'w') # add .mp4 file extension
     video_stream = output_container.add_stream(template=video_stream)
-    audio_stream = output_container.add_stream(template=audio_stream)
+    if audio_stream != None:
+        audio_stream = output_container.add_stream(template=audio_stream)
 
     start_pts = None
     for packet in input_container.demux():
